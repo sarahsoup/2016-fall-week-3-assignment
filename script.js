bee7adb605c92735b388b698dd355212f9ab0c13
Sarah Campbell <sarahsoup@Sarahs-MacBook-Pro.local>
2016-09-29 23:31:26 -0400

    week 3 assignment

--- script.js
+++ script.js
@@ -14,31 +14,64 @@ var productList = []; //this is an array
 //Your code here
 //Hint: productList.push(newObject) will add the object you just created to the array
 
-console.log(productList[0]);
+var shoeProd1 = {
+ name:"ASICS Men's Gel Venture 5 Running Shoe",
+ price:79.95,
+ maker:"ASICS"
+};
+
+var shoeProd2 = {
+ name:"JACKSHIBO Men Women Unisex Couple Casual Fashion Sneakers Breathable Athletic Sports Shoes",
+ price:32.99,
+ maker:"JACKSHIBO"
+};
+
+var shoeProd3 = {
+name:"Champion Women's Gusto Cross Trainer",
+price:39.97,
+maker:"Champion"
+};
+
+var shoeProd4 = {
+name:"PUMA Adult Suede Classic Shoe",
+price:168.32,
+maker:"PUMA"
+};
+
+productList.push(shoeProd1, shoeProd2, shoeProd3, shoeProd4);
+
+console.table(productList);
 
 /* Step 2
  * Iterate (or go through) productList one by one using the for loop
  * and for each product, if the price is below $50, console.log a message like below
  * "The product [name] costs [price], and is a match"
  */
+
+ /* Step 3
+  * What is the average price of all the products?
+  */
+
+ //3.1 first, calculate total price using the for loop again
+
+ //3.2 then, divide total price by the number of products, using proudctList.length
+
+var sum = 0;
+
 for(var i = 0; i < productList.length; i++){
+  sum = sum + productList[i].price;
+  if (productList[i].price < 50) {
+    console.log("Product " + productList[i].name + " costs " + productList[i].price + ", and is a match")
+  };
 
-    //Your code here
-
-    console.log();
 }
 
-/* Step 3
- * What is the average price of all the products?
- */
+var average = sum / productList.length;
 
-//3.1 first, calculate total price using the for loop again
-
-//3.2 then, divide total price by the number of products, using proudctList.length
-
+console.log("The average price of the " + productList.length + " products is $" + average);
 
 /* Step 4
- * Write a function that accept a single argument, price
+ * Write a function that accepts a single argument, price
  * so that the function returns the number of products below that price
  */
 
@@ -46,10 +79,13 @@ function comparePrice(p){
 
     var numOfMatchingProducts = 0;
 
-    //Your code here
-
-    return numOfMatchingProducts;
+for(var i = 0; i < productList.length; i++){
+  if(productList[i].price < p) {
+    numOfMatchingProducts++;
+  };
 }
+    return numOfMatchingProducts;
+};
 
 //
 console.log("The number of products below $10 is " + comparePrice(10));
